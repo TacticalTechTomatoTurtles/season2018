c7975abf1f345c32ae2753dba988b9d36e36f669
afisher2 <afisher1@gmail.com>
2018-10-27 10:42:53 -0500

    recent updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBase.java
@@ -5,6 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class BlueBase extends LinearOpMode {
     @Override
     public void runOpMode() {
+        // go forward x inches
+
+        // turn right 90 degrees
+
+        // go forward y inches
+
+        // drop the figure
+
 
     }
 }
